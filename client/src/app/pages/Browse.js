e9928a2647ee385451396c4b3fda334bb0ecab80
pgm-cagravci <cagravci@student.arteveldehs.be>
2021-06-10 14:09:51 +0200

    added dropdown filter to choose on the browse page series or movies

--- client/src/app/pages/Browse.js
+++ client/src/app/pages/Browse.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import { BaseLayout } from '../layouts';
 import PopularMovies from '../components/movies/PopularMovies'
 import UpComingMovies from '../components/movies/UpComingMovies';
@@ -8,16 +9,33 @@ import UpComingSeries from '../components/series/UpComingSeries';
 import TopRatedSeries from '../components/series/TopRatedSeries';
 
 const Browse = () => {
+    const [typeFilter, setTypeFilter] = useState('movie');
+
+    function handleChange(e) {
+        const type = e.target.value
+        setTypeFilter(type);
+    }
+
   return (
     <BaseLayout>
-      <h1>Browse movies: </h1>
+        <select onChange={handleChange}>
+            <option>movie</option>
+            <option>serie</option>
+        </select>
+
+        {typeFilter === 'movie' && <h1>Browse movies: </h1> || <h1>Browse series: </h1>}
+        {typeFilter === 'movie' && <PopularMovies /> || <PopularSeries />}
+        {typeFilter === 'movie' && <UpComingMovies /> ||  <UpComingSeries />}
+        {typeFilter === 'movie' && <TopRatedMovies /> || <TopRatedSeries />}
+      
+      {/* <h1>Browse movies: </h1>
       <PopularMovies />
       <UpComingMovies />
       <TopRatedMovies />
       <h1>Browse series: </h1>
       <PopularSeries />
       <UpComingSeries />
-      <TopRatedSeries />
+      <TopRatedSeries /> */}
     </BaseLayout>
   );
 };
