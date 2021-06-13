c4f839fdaa5319d1c16e0ecf7bce87cacbbe1e10
pgm-cagravci <cagravci@student.arteveldehs.be>
2021-06-13 23:00:44 +0200

    minor changes

--- client/src/app/components/layout/Search.js
+++ client/src/app/components/layout/Search.js
@@ -17,7 +17,7 @@ const Search = (e) => {
         if(search) {
             const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`);
             const data = await response.json();
-            setData(data.results);
+            data.results && setData(data.results);
         }
     }
 
@@ -28,7 +28,7 @@ const Search = (e) => {
 
     return (
         <div className={styles.Search}>
-            <input type="text" placeholder="search movie..." onChange={handleChangeSearch} value={search} />
+            <input type="text" placeholder="find movie..." onChange={handleChangeSearch} value={search} />
             <button type="submit">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22.224" height="22.228" viewBox="0 0 22.224 22.228">
                     <path id="Icon_awesome-search" data-name="Icon awesome-search" d="M21.922,19.218,17.594,14.89a1.041,1.041,0,0,0-.738-.3h-.708a9.025,9.025,0,1,0-1.563,1.563v.708a1.041,1.041,0,0,0,.3.738l4.328,4.328a1.038,1.038,0,0,0,1.472,0l1.229-1.229a1.047,1.047,0,0,0,0-1.476ZM9.029,14.586a5.556,5.556,0,1,1,5.556-5.556A5.553,5.553,0,0,1,9.029,14.586Z" fill="#f9f9f9"/>
